f7a5fc442cae965dd1ae0a2bc366df041a5820eb
clintomed <clintonmedbery@BigMac.local>
2015-08-15 09:34:55 -0400

    Started NSOperation for data sync

---
+++ HeartSync/HeartBeatDataOperations.swift
@@ -0,0 +1,9 @@
+//
+//  HeartBeatDataOperations.swift
+//  HeartSync
+//
+//  Created by Clinton Medbery on 8/10/15.
+//  Copyright (c) 2015 Programadores Sans Frontieres. All rights reserved.
+//
+
+import Foundation
